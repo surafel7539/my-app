72e8f82fc9e3c1878c90d4f0c100fddd78014b59
surafel7539 <suchaz7539@gmail.com>
2026-09-18 15:49:58 +0300

    i

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import ExploreBtn from '@/components/ExploreBtn'
 import { events } from '@/lib/contants'
 import React from 'react'
 
-
+export const instance = false
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 const page = async () => {
